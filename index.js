ce4f34729a90a7420d2037d9e3cff6d45dcd9f0b
Karol <19karol98@gmail.com>
2022-12-21 21:09:30 +0100

    3_8

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan("tiny"));
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
@@ -80,7 +83,6 @@ app.post("/api/persons", (request, response) => {
   };
 
   persons = persons.concat(person);
-
   response.json(person);
 });
 
