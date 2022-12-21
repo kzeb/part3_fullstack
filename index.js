40fa74363b8a5a7e2433f564191f055d771192e9
Karol <19karol98@gmail.com>
2022-12-21 16:46:36 +0100

    3_2

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  response.send(`Phonebook has info for ${persons.length} people <br><br>
+    ${new Date(Date.now()).toString()}`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
