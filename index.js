772b701cb8490464620dae962ccc433b39027e82
Karol <19karol98@gmail.com>
2022-12-23 10:13:34 +0100

    3_16

--- index.js
+++ index.js
@@ -43,14 +43,16 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
@@ -67,10 +69,6 @@ app.get("/info", (request, response) => {
     ${new Date(Date.now()).toString()}`);
   });
 });
-
-// const generateId = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// };
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -96,6 +94,24 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson);
   });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
